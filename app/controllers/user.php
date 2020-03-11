b088f044e300283476731b36ddd54f77dfecd1c7
pwnz22 <parliament888@gmail.com>
2020-03-11 14:25:15 +0500

    init

---
+++ app/controllers/user.php
@@ -0,0 +1,14 @@
+<?php
+
+class User extends Controller
+{
+    public function index()
+    {
+        echo 'user/index111';
+    }
+
+    public function www()
+    {
+        echo 'www';
+    }
+}
